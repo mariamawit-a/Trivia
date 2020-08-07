591d005eb0bb117b04cf6b46ebb7d6eaff2f5d21
mariamawit-a <63439092+mariamawit-a@users.noreply.github.com>
2020-08-06 19:30:02 -0700

    fixing choices rendering

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 //const axios = require('axios');
 const path = require('path');
 const app = express();
+var $ = require('jquery');
 const PORT = process.env.PORT || 3000
 
 //app.set('views', path.join(__dirname, 'views'));
