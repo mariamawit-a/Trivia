76333cd4f7e205e1494541deca342808fd0270dd
mariamawit-a <63439092+mariamawit-a@users.noreply.github.com>
2020-08-06 23:02:56 -0700

    timer working

---
+++ routes/games.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+
+
+
+router.get('/', async (req, res) =>{
+    
+      const reponse = await axios.get('https://opentdb.com/api.php?amount=26&difficulty=easy&type=multiple');
+       
+      
+        res.render('games/index', {value: reponse.data.results});
+    
+      
+});
+
+/* GET movie by id listing. */
+router.get('/:id', async (req, res, next) => {
+    const { id } = req.params;
+  
+    try {
+      const { data: question } = await axios.get(
+        `https://opentdb.com/api.php?amount=${id}&difficulty=easy&type=multiple`,
+      );
+      // const movieData = movie.data;
+      console.log(movie);
+      res.render('games/question', {
+        question,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  });
+  
+  module.exports = router;
