708e293ac2778219f78c597bbcb1bd57bec4e24b
mariamawit-a <63439092+mariamawit-a@users.noreply.github.com>
2020-08-19 01:09:24 -0700

    added party room

--- routes/games.js
+++ routes/games.js
@@ -1,24 +1,55 @@
 const express = require('express');
 const axios = require('axios');
+const { response } = require('express');
 const router = express.Router();
+const generate = require('meaningful-string');
 
-router.use(express.urlencoded({extended: true}))
 
-router.get('/', async (req, res) =>{
+// router.use(express.urlencoded({extended: true}))
+
+// router.get('/', async (req, res) =>{
     
-      const reponse = await axios.get('https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple');
+//       const response = await axios.get('https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple');
        
-      res.render('intro/level', {questions: reponse.data.results});   
-});
+//       res.render('intro/level', {questions: response.data.results});   
+// });
+router.get('/party', async(req, res, next) => {
 
+  const token = await axios.get(
+    `https://opentdb.com/api_token.php?command=request`,
+  );
+    const key = token.data.token;
+
+  try {
+    const response1 = await axios.get(
+      `https://opentdb.com/api.php?amount=50&type=multiple&token=${key}`,
+    );
+    const response2 = await axios.get(
+      `https://opentdb.com/api.php?amount=50&type=multiple&token=${key}`,
+    );
+
+    let item1 = response1.data.results;
+    let item2 = response2.data.results;
+
+    console.log(typeof(item1), item1.length);
+    console.log(typeof(item2), item2.length);
+    
+
+    res.render('games/party', {questions1: item1, questions2 : item2});
+  } catch (e) {
+    console.log(e);
+  }
+
+
+});
 router.get('/:categoryid', async (req, res, next) => {
     const {categoryid} = req.params;
   
     try {
-      const reponse = await axios.get(
-        `https://opentdb.com/api.php?amount=10&category=${categoryid}&type=multiple`,
+      const response = await axios.get(
+        `https://opentdb.com/api.php?amount=20&category=${categoryid}&type=multiple`,
       );
-      res.render('games/index', {questions: reponse.data.results});
+      res.render('games/index', {questions: response.data.results});
     } catch (e) {
       console.log(e);
     }
@@ -29,29 +60,39 @@ router.get('/:categoryid', async (req, res, next) => {
     const {categoryid} = req.params;
   
     try {
-      const reponse = await axios.get(
-        `https://opentdb.com/api.php?amount=10&category=${categoryid}&type=multiple`,
-      );
-      res.render('games/chill', {questions: reponse.data.results});
+        const response = await axios.get(
+          `https://opentdb.com/api_count.php?category=${categoryid}&type=multiple`,
+        );
+        var number = response.data.category_question_count.total_question_count;
+
+          try {
+            const reponse = await axios.get(
+              `https://opentdb.com/api.php?amount=${number}&category=${categoryid}&type=multiple`,
+            );
+            res.render('games/chill', {questions: reponse.data.results});
+          } catch (e) {
+            console.log(e);
+          }
     } catch (e) {
       console.log(e);
     }
 
   });
 
-router.get('/:categoryid/:difficultyLevel', async (req, res, next) => {
-  const {categoryid} = req.params;
-  const {difficultyLevel} = req.params;
+  
+// router.get('/:categoryid/:difficultyLevel', async (req, res, next) => {
+//   const {categoryid} = req.params;
+//   const {difficultyLevel} = req.params;
 
-  try {
-    const reponse = await axios.get(
-      `https://opentdb.com/api.php?amount=10&category=${categoryid}&difficulty=${difficultyLevel}&type=multiple`,
-    );
-    res.render('games/index', {questions: reponse.data.results});
-  } catch (e) {
-    console.log(e);
-  }
-});
+//   try {
+//     const response = await axios.get(
+//       `https://opentdb.com/api.php?amount=10&category=${categoryid}&difficulty=${difficultyLevel}&type=multiple`,
+//     );
+//     res.render('games/index', {questions: response.data.results});
+//   } catch (e) {
+//     console.log(e);
+//   }
+// });
 
 router.post('/submit/:timeleft', async(req, res, next) => {
 
@@ -71,5 +112,26 @@ router.post('/submit/:timeleft', async(req, res, next) => {
 
   res.render('games/score', {score:score, time: minutes.toString().padStart(2, 0)+":"+ seconds.toString().padStart(2, 0)});
 });
+
+router.post('/party/submit', async(req, res, next) => {
+
+  
+  let right = 0;
+  let wrong = 0;
+  console.log(req.body);
+  Object.values(req.body).forEach(answer =>{
+
+    if(answer=='correct')
+      right++;
+    else if(answer=='incorrect')
+      wrong++;
+  });
+
+  const total = 2*right-wrong;
+
+  res.render('games/share', {right, wrong, total});
+});
+
+
   
   module.exports = router;
